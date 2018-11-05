4137a8309fd55769b4ec4b085005ad52cdea3a82
gummisig94 <gudmundurs@hotmail.com>
2018-11-05 12:45:38 +0100

    missing

--- src/com/DTU/PrintClient.java
+++ src/com/DTU/PrintClient.java
@@ -4,14 +4,15 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class PrintClient {
+public class PrintClient{
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
         Printerface print = (Printerface) Naming.lookup("rmi://localhost:6969/print");
         System.out.println("-- " + print.Echo("Hey Server"));
     }
 
-    public void print() throws RemoteException{
+    public void print(String filename, String printer) throws RemoteException{
 
     }
 
